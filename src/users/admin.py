7f39c2ca59d4cec0a61aa705b93cad06633a45c3
Amir Mohseni <amir@Amirs-MacBook-Air.local>
2025-09-29 16:04:08 +0330

    chore(users): update admin registration/config

--- src/users/admin.py
+++ src/users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'user_type', 'is_staff')
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        (None, {'fields': ('user_type',)}),
+    )
+    add_fieldsets = DjangoUserAdmin.add_fieldsets + (
+        (None, {'fields': ('user_type',)}),
+    )
