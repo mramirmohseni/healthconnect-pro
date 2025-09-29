5d0e880b02d4c38390f7d97d47beeb8c3045ee60
Amir Mohseni <amir@Amirs-MacBook-Air.local>
2025-09-29 16:03:48 +0330

    feat(users): update user models

--- src/users/models.py
+++ src/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('patient', 'Patient'),
+        ('doctor', 'Doctor'),
+    ]
+    user_type = models.CharField(max_length=10,
+        choices=USER_TYPE_CHOICES, 
+        default='patient'
+    )
+    
+    def __str__(self):
+        return self.get_full_name() or self.username or self.email or f'User {self.pk}'
+    
